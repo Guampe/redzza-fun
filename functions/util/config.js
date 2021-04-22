69bb0426b2c809dfde7083c40cb40290299800f3
Guampe <law.rel@hotmail.com>
2021-04-22 17:31:48 -0500

    first commit

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyCiPFgQ-cc37x2RchPIXs3XTjkZTPYz7dE",
+  authDomain: "redzza-e4105.firebaseapp.com",
+  projectId: "redzza-e4105",
+  storageBucket: "redzza-e4105.appspot.com",
+  messagingSenderId: "498157489411",
+  appId: "1:498157489411:web:737da5c08d605dda35ad36",
+  measurementId: "G-9L7LPNHZWY"
+};
